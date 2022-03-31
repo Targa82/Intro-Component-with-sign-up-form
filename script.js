b480647920309e3ac727b0d800a9b552e67feba8
Targa82 <101151467+Targa82@users.noreply.github.com>
2022-03-31 15:39:44 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 const btn = document.querySelector(".form-button");
 const inputField = document.querySelectorAll(".input");
 const form = document.querySelector(".form-container");
+const email = document.getElementById("email");
+const pattern = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+const errorRem = function (er) {
+  er.classList.remove("error");
+  er.nextElementSibling.classList.remove("visible");
+};
+const errorAdd = function (er) {
+  er.classList.add("error");
+  er.nextElementSibling.classList.add("visible");
+};
 
 btn.addEventListener("click", function (e) {
   let i = 0;
   e.preventDefault();
   inputField.forEach((element) => {
-    element.classList.remove("error");
-    element.nextElementSibling.classList.remove("visible");
-
-    if (element.value === "") {
-      element.classList.add("error");
-      element.nextElementSibling.classList.add("visible");
+    errorRem(element);
+    if (element.value.trim() === "") {
+      errorAdd(element);
+      element.placeholder = "";
+    } else if (!email.value.match(pattern)) {
+      errorAdd(email);
+      email.value = " Wrong E-mail Format";
+      email.style.color = "red";
     } else i++;
     console.log(i);
     if (i === 4) {
       form.submit();
     }
+  });
+});
+
+inputField.forEach((el) => {
+  el.addEventListener("focus", function () {
+    el.value = "";
+    errorRem(el);
+    el.style.color = "black";
   });
 });
